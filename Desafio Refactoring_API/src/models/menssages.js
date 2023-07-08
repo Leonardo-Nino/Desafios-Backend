8be28e621755ce6b952c7395b826f16132b5a031
Leonardo Niño <leonardoninio@gmail.com>
2023-07-08 18:37:43 +0300

    Delete Desafio Refactoring_API directory

--- Desafio Refactoring_API/src/models/menssages.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from 'mongoose'
-
-const messagesSchema = new Schema({
-  user: String,
-  messages: String,
-})
-
-export const messagesModel = model('messages', messagesSchema)
--- Desafio Refactoring_API/src/utils/bcrypt.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcrypt'
-
-const salt = bcrypt.genSaltSync(parseInt(process.env.SALT))
-
-export const hashPassword = async (password) => bcrypt.hash(password, salt)
-
-export const validatePassword = async (password, hash) =>
-  bcrypt.compare(password, hash)

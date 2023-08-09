e96794125c8cf6814e400a89997f807f07b1d1f0
Leonardo <leonardoninio@gmail.com>
2023-08-09 12:41:12 +0300

    first commit

---
+++ Desafio_complementario_3/src/DAL/DAOs/mongoDAO/registerMongo.js
@@ -0,0 +1,18 @@
+import { userModel } from '../../mongoDB/models/user.js'
+import { hashPassword } from '../../../utils/bcrypt.js'
+
+//create new user
+
+export const createUser = async (obj) => {
+  try {
+    const hashPass = await hashPassword(obj.password)
+
+    const newObj = { ...obj, password: hashPass }
+
+    const newUser = await userModel.create(newObj)
+
+    return newUser
+  } catch (error) {
+    return error
+  }
+}
